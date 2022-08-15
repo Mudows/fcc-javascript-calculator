d8a38e517f78dbcf6de5a13b9e1f831c2ea79a5b
Diego Moraes Cezar <dmcezar@gmail.com>
2022-08-15 14:43:44 -0300

    FEAT: Included link to my portfolio page.

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -10,7 +10,7 @@ class Calculator extends React.Component {
         <span>Mudow's JS Calculator</span>
         <Display />
         <Keyboard />
-        <div id="signature">Designed and coded by Diego M. Cezar</div>
+        <div id="signature"><a href="https://mudows.github.io/" target="_blank" rel="noreferrer">Designed and coded by Diego M. Cezar</a></div>
       </div>
     )
   }
